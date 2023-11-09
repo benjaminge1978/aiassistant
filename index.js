5af50192ab80598cba364e97a777668ace3ce31f
Ben Davidson <bendavidson@MacBook-Pro-3.local>
2023-11-09 13:22:40 +0100

    Fix OpenAI API call method3333

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { Server } = require("socket.io");
-const { OpenAI } = require('openai'); // Corrected import
+const { Configuration, OpenAIApi } = require('openai');
 const pdf = require('pdf-parse');
 const fs = require('fs');
 const http = require('http');
@@ -32,19 +32,23 @@ pdf(dataBuffer).then(function(data) {
   console.error('Error reading PDF:', err);
 });
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+// Set up configuration for OpenAI
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 async function getOpenAIResponse(question) {
     const prompt = `The following is a question from a user:\n"${question}"\n\nThe context from the PDF is as follows:\n${pdfText}\n\nThe answer is:`;
   
     try {
-        const response = await openai.Completion.create({
+        const response = await openai.createCompletion({
         model: "text-davinci-003", // Replace with your model of choice
         prompt: prompt,
         max_tokens: 150
       });
   
-      return response.choices[0].text.trim();
+      return response.data.choices[0].text.trim();
     } catch (error) {
       console.error('Error calling OpenAI API:', error);
       return "I'm sorry, I encountered an error while fetching the response.";
