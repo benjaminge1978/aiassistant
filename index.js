081ee13b265a7dc8a3caf82b923cafd3aeca189f
Ben Davidson <bendavidson@MacBook-Pro-3.local>
2023-11-09 13:08:32 +0100

    Fix OpenAI API call method

--- index.js
+++ index.js
@@ -35,21 +35,21 @@ pdf(dataBuffer).then(function(data) {
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 async function getOpenAIResponse(question) {
-  const prompt = `The following is a question from a user:\n"${question}"\n\nThe context from the PDF is as follows:\n${pdfText}\n\nThe answer is:`;
-
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 150
-    });
-
-    return response.choices[0].text.trim();
-  } catch (error) {
-    console.error('Error calling OpenAI API:', error);
-    return "I'm sorry, I encountered an error while fetching the response.";
+    const prompt = `The following is a question from a user:\n"${question}"\n\nThe context from the PDF is as follows:\n${pdfText}\n\nThe answer is:`;
+  
+    try {
+      const response = await openai.Completion.create({
+        model: "text-davinci-003", // Replace with your model of choice
+        prompt: prompt,
+        max_tokens: 150
+      });
+  
+      return response.choices[0].text.trim();
+    } catch (error) {
+      console.error('Error calling OpenAI API:', error);
+      return "I'm sorry, I encountered an error while fetching the response.";
+    }
   }
-}
 
 io.on('connection', (socket) => {
   socket.on('disconnect', () => {
